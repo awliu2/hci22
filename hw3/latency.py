6179ee7d42f9c6e308771db676fdc2cd38361e6d
Andi Willow Liu <awliu2@uchicago.edu>
2022-10-05 23:07:59 -0500

    completed hw3

--- hw3/latency.py
+++ hw3/latency.py
@@ -4,8 +4,7 @@ from random import uniform
 import time
 from datetime import datetime
 
-def main():
-    
+def main():    
     with open('out.csv', "w") as f:
         for i in range(30):
             os.system('clear')
@@ -15,11 +14,8 @@ def main():
             input("\r press enter")
             tend = datetime.now()
 
-            
             f.write(str(int((tend - tstart).total_seconds() * 1000)))
-            if i < 29:
-                f.write(", ") 
-
+            if i < 29: f.write(", ") 
 
 if __name__ == "__main__":
     main()
