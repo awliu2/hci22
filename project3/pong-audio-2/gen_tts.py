c5488b4368c73ede085783c8a6aa1d01dbff26b9
awliu2 <awliu2@obamas-wiiU.local>
2022-11-30 14:24:39 -0600

    added tts file

---
+++ project3/pong-audio-2/gen_tts.py
@@ -0,0 +1,19 @@
+from gtts import gTTS
+
+pause_speech = "Game paused. Say play to resume"
+easy_speech = "difficulty: easy"
+hard_speech = "difficulty: hard"
+insane_speech = "difficulty: insane"
+
+pause_tts = gTTS(text=pause_speech, lang='en', slow=False)
+easy_tts = gTTS(text=easy_speech, lang='en', slow=False)
+hard_tts = gTTS(text=hard_speech, lang='en', slow=False)
+insane_tts = gTTS(text=insane_speech, lang='en', slow=False)
+
+pause_tts.save("pause.mp3")
+
+easy_tts.save("easy.mp3")
+
+hard_tts.save("hard.mp3")
+
+insane_tts.save("insane.mp3")
